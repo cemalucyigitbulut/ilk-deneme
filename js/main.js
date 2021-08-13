b1ed5e9597a93ccc1f8bcabfbbbd781dd645ec6b
unknown <cemalyigit123@hotmail.com>
2021-08-13 12:36:21 +0300

    /dunno

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Log in
 const model = document.querySelector('.model')
 const loginn = document.querySelector('.loginn')
 const closee = document.querySelector('.close')
@@ -5,6 +6,7 @@ const closee = document.querySelector('.close')
 loginn.addEventListener('click',openModel)
 closee.addEventListener('click',closeModel)
 window.addEventListener('click',outsideClick)
+
 
 function openModel(){
     model.style.display ='block'  //.model display:none; nu burdan manupule ettik//
@@ -19,3 +21,35 @@ function outsideClick(e){     //(e) = event paramater//
        closeModel()
    }
 }
+
+
+
+//Details 
+const cemdetials = document.querySelector('.cemdetailsopen')
+const cemopen = document.querySelector('.cemdetails')
+const close22 = document.querySelector('.close2')
+
+cemopen.addEventListener('click',openModel2)
+close22.addEventListener('click',closeModel2)
+window.addEventListener('click',outsideClick2)
+
+function openModel2(){
+    cemdetials.style.display='block'
+}
+
+function closeModel2(){
+    cemdetials.style.display='none'
+}
+
+function outsideClick2(e){
+    if(e.target == cemdetials){
+        closeModel2()
+    }
+}
+
+
+
+
+//live buttons
+
+
